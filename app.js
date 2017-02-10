903044a668add2fabd2379dfff275c6b5492dd16
vinicius <vinitecnico@gmail.com>
2017-02-09 22:19:18 -0200

    ajuste code

--- app.js
+++ app.js
@@ -4,27 +4,26 @@ var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 var url = 'mongodb://admin:v8twanzo@ds056789.mlab.com:56789/teste';
 var agenda = new Agenda({ db: { address: url } });
 
-MongoClient.connect(url, function (err, db) {
-    var collection = db.collection('test');
+agenda.define('payschedule', function (job, done) {
+    console.log(job);
+    console.log(done);
+    remove();
+});
 
-    agenda.define('payschedule', function (job, done) {
-        var data = job.attrs.data;
-        collection.remove({ 'email': "luanna@gmail.com" }, done);
-    });
-
-    agenda.on('ready', function () {
-        //   agenda.every('3 minutes', 'payschedule'); 
-        // Alternatively, you could also do: 
-        agenda.every('*/3 * * * *', 'payschedule');
-
-        agenda.start();
-    });
-
-    console.log("Connected correctly to server");
-    //db.close();
+agenda.on('ready', function () {
+    agenda.every('*/5 * * * *', 'payschedule');
+    agenda.start();
 });
 
 
+function remove() {
+    MongoClient.connect(url, function (err, db) {
+        var collection = db.collection('test');
+        collection.remove({ email: 'luanna@gmail.com' });
+        console.log("Connected correctly to server");
+        db.close();
+    });
+}
 
 
 
